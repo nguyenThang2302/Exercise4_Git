8c35587554d0d5235343de0a89265fc9323c1d19
Nguyen Van Thang <thangnv@tech.est-rouge.com>
2023-12-21 13:46:18 +0700

    BAPI-01: Add file listpost.js

---
+++ listpost.js
@@ -0,0 +1,27 @@
+const listPost = [
+    {
+        "id": "1",
+        "name": "Blog1",
+        "description": "Description blog 1",
+        "createdAt": "xxx",
+        "updatedAt": "yyy",
+        "deletedAt": "zzz"
+    },
+    {
+        "id": "2",
+        "name": "Blog2",
+        "description": "Description blog 2",
+        "createdAt": "xxx",
+        "updatedAt": "yyy",
+        "deletedAt": "zzz"
+    },
+    {
+        "id": "3",
+        "name": "Blog3",
+        "description": "Description blog 3",
+        "createdAt": "xxx",
+        "updatedAt": "yyy",
+        "deletedAt": "zzz"
+    }
+];
+
